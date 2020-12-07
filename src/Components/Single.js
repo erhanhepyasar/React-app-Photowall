e8e6d36259e2efed41f135612fadf96a1e8d372d
Erhan Hepyaşar <200189@gm.isbank.com.tr>
2020-12-07 13:02:11 +0300

    fix

--- src/Components/Single.js
+++ src/Components/Single.js
@@ -9,12 +9,19 @@ class Single extends Component {
         const post = posts.find((post) => post.id === id)            
         const comments = this.props.comments[match.params.id] || []
         const index = this.props.posts.findIndex((post) => post.id === id)
-        return (
-            <div className="single-photo">
-                <Photo post={post} {...this.props} index={index}/>
-                <Comments startAddingComments={this.props.startAddingComments} comments={comments} id={id}/>
-            </div>
-        )
+        
+        if (this.props.loading === true) {
+            return <div className="loader"> ...loading </div>
+        } else if(post) {
+            return (
+                <div className="single-photo">
+                    <Photo post={post} {...this.props} index={index}/>
+                    <Comments startAddingComments={this.props.startAddingComments} comments={comments} id={id}/>
+                </div>
+            )
+        } else {
+            return <h1> ... no post found </h1>
+        }
     }
 }
 
